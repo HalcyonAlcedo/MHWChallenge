c7f3cfc433f7de9504416c7dad9370ae618ce74a
HalcyonAlcedo <zyc404@gmail.com>
2021-10-08 11:30:29 +0800

    更新依赖

--- vue.config.js
+++ vue.config.js
@@ -21,10 +21,10 @@ module.exports = {
 
   pluginOptions: {
     i18n: {
-      locale: undefined,
-      fallbackLocale: undefined,
-      localeDir: undefined,
-      enableInSFC: undefined,
+      locale: 'zh_cn',
+      fallbackLocale: 'en',
+      localeDir: 'lang',
+      enableInSFC: false,
     },
   },
 }
